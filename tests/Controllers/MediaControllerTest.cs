3f25f928b65c584eab43da50ab92a6271300569f
= <aguibw@gmail.com>
2024-09-02 05:07:17 +0100

    Fixed some tests

--- tests/Controllers/MediaControllerTest.cs
+++ tests/Controllers/MediaControllerTest.cs
@@ -27,18 +27,5 @@ namespace Ecommerce.Test.Controllers
 
         }
 
-        [Theory]
-        [InlineData("walid")]
-        public async void MediaController_CreateFiles_ReturnsCreated(string id)
-        {
-            var files = A.Fake<IFormFileCollection>();
-            UserManager<User> manager = A.Fake<UserManager<User>>();
-            var controller = new MediaController(media, manager);
-
-            var result = await controller.UploadMultiFiles(files, id);
-
-            result.Should().BeOfType(typeof(OkObjectResult));
-
-        }
     }
 }
